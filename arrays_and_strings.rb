c3d3ae3b2857c2df1cd549fef4cac992d1986c95
Porfirio Matias <SSJ6Porfy@hotmail.com>
2017-11-28 22:55:36 -0800

    is_perm and urlify complete

--- arrays_and_strings.rb
+++ arrays_and_strings.rb
@@ -1,4 +1,4 @@
-
+# Problem (1)
 def unique_string(str)
     letters = {}
 
@@ -8,6 +8,7 @@ def unique_string(str)
     end
 
     true
+    # O(n) complexity
     # if other data structures weren't allowed
     # one could use a quadratic function to compare chars
     # or sort and compare pairs
@@ -16,3 +17,21 @@ end
 # str = 'abcdef'
 # p unique_string(str)
 
+# Problem (2)
+
+def is_Permutation?(str1, str2)
+    str1.chars.sort.join == str2.chars.sort.join
+    # O(log n) complexity
+end
+
+ex1 = 'abcdef'
+ex2 = 'cbafed'
+
+p is_Permutation?(ex1, ex2)
+
+def urlify(str)
+    str.split(' ').join('%20')
+end
+
+str = 'Mr John Smith    '
+p urlify(str)
