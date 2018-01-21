6913d37f37cb49be73508a48b64ed5196fc64352
Porfirio Matias <SSJ6Porfy@hotmail.com>
2018-01-20 23:47:54 -0800

    added stacks and queues

---
+++ Cracking the Coding Interview/stacks_and_queues.js
@@ -0,0 +1,27 @@
+function Stack() {
+    this.store = [];
+
+    this.push = (val) => {
+        this.store.push(val);
+    };
+
+    this.pop = () => {
+        let val = this.store.pop();
+        return val;
+    };
+
+    this.peek = () => {
+        console.log(this.store[this.store.length - 1]);
+        return this.store[this.store.length - 1];
+    };
+}
+
+let testStack = new Stack();
+
+testStack.push(1);
+testStack.peek();
+testStack.push(2);
+testStack.peek();
+testStack.push(1);
+let val = testStack.pop();
+testStack.peek();
